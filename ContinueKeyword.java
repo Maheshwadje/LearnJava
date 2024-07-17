dc6352a916d44ff337ed8575eaa097724c481fdc
Mahesh Wadje <161317925+Maheshwadje@users.noreply.github.com>
2024-07-17 23:12:44 +0530

    Add files via upload

---
+++ ContinueKeyword.java
@@ -0,0 +1,16 @@
+package concept;
+//it is used to skip any particular iteration
+public class ContinueKeyword 
+{
+	public static void main(String[] args)
+	{
+		for(int i = 0; i<=8 ; i++)
+		{
+			if(i==6)
+			{
+				continue;
+			}
+			System.out.println("My kids age is " +i);
+		}
+	}
+}
